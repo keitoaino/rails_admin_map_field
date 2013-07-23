30fe0a44da4a2cae88bd6b9988d7a6ee78942ae6
Eugene Udovychenko <udovychenko@mlsdev.com>
2013-07-23 15:48:12 +0300

    Done pre-version of plugin\

--- app/assets/javascript/map.coffee
+++ app/assets/javascript/map.coffee
@@ -1,9 +1,43 @@
+address = document.getElementById window.address
+lat = document.getElementById window.lat
+lng = document.getElementById window.lng
+
+geocoder = new google.maps.Geocoder()
+marker = null
+
 mapOptions = {
   zoom: 8,
-  center: new google.maps.LatLng(40.714353, -74.005973),
   mapTypeId: google.maps.MapTypeId.ROADMAP
 }
 
-map = new google.maps.Map document.getElementById('map'), mapOptions
+map_element = document.getElementById 'map'
+map = new google.maps.Map map_element, mapOptions
 
 google.maps.event.addListener map, 'click', (event) ->
+  updateLocation event.latLng
+
+updateLocation = (location, center) ->
+  if marker
+    marker.setPosition location
+  else
+    marker = new google.maps.Marker
+      position: location,
+      map: map
+
+  lat.value = location.lat()
+  lng.value = location.lng()
+
+  if center
+    map.setCenter location
+
+setInterval ->
+  google.maps.event.trigger map, 'resize'
+  , 100
+
+address.addEventListener 'keyup', ->
+  geocoder.geocode {address: this.value}, (results, status) ->
+    if status == google.maps.GeocoderStatus.OK
+      updateLocation results[0].geometry.location, true
+
+
+updateLocation new google.maps.LatLng(lat.value, lng.value), true
--- lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
+++ lib/rails_admin_map_field/rails_admin/config/fields/types/map.rb
@@ -3,50 +3,40 @@ module RailsAdmin::Config::Fields::Types
     RailsAdmin::Config::Fields::Types::register(:map, self)
 
     def allowed_methods
-      [@name, longitude_field]
+      [@name, address_field, lat_field, lng_field]
     end
 
-    # THe name of the corresponding longitude field to match the latitude field
-    # in this object.
-    register_instance_option(:longitude_field) do
-      :longitude
+    register_instance_option :address_field do
+      :address
     end
+
+    register_instance_option :lat_field do
+      :lat
+    end
+
+    register_instance_option :lng_field do
+      :lng
+    end
+
 
     register_instance_option(:partial) do
       :form_map
     end
 
-    # Google Maps API Key - optional
     register_instance_option(:google_api_key) do
       nil
     end
 
-    # Latitude value to display in the map if the latitude attribute is nil
-    # (Otherwise the location defaults to (0,0) which is in the Gulf of Guinea
-    register_instance_option(:default_latitude) do
-      51.5 # Latitude of London, United Kingdom
+    def address_dom_name
+      @dom_name ||= "#{bindings[:form].object_name}_#{address_field}"
     end
 
-    # Longitude value to display if the longitude attribute is nil
-    register_instance_option(:default_longitude) do
-      -0.126 # Longitude of London, United Kingdom
+    def lat_dom_name
+      @lat_dom_name ||= "#{bindings[:form].object_name}_#{lat_field}"
     end
 
-    # Default zoom level of the map
-    register_instance_option(:default_zoom_level) do
-      8
-    end
-
-    def dom_name
-      @dom_name ||= "#{bindings[:form].object_name}_#{@name}_#{longitude_field}"
-    end
-
-    def latitude_dom_name
-      @lat_dom_name ||= "#{bindings[:form].object_name}_#{@name}"
-    end
-
-    def longitude_dom_name
-      @lon_dom_name ||= "#{bindings[:form].object_name}_#{longitude_field}"
+    def lng_dom_name
+      @lng_dom_name ||= "#{bindings[:form].object_name}_#{lng_field}"
     end
   end
 end
